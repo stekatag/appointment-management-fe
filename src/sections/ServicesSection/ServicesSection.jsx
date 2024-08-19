e95132e997eb95404ca602122d5d548828911a21
Stefan Gogov <stefangogov@proton.me>
2024-08-19 14:32:25 +0300

    refactor: Update Services Section to include data from the DB

--- src/sections/ServicesSection/ServicesSection.jsx
+++ src/sections/ServicesSection/ServicesSection.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import {
   Container,
@@ -9,6 +9,7 @@ import {
   Link,
   useTheme,
   useMediaQuery,
+  Alert,
 } from "@mui/material";
 import {
   ServicesContainer,
@@ -22,74 +23,31 @@ import {
   CustomTabs,
 } from "./ServicesSection.styles";
 import ScrollAnimation from "react-animate-on-scroll";
-
-const servicesData = {
-  Haircuts: [
-    {
-      title: "Classic Haircut",
-      description:
-        "A timeless haircut that suits all occasions, providing a clean and polished look. Includes a professional cut and style.",
-      price: "$15.00",
-    },
-    {
-      title: "Haircut & Shampoo",
-      description:
-        "A comprehensive package that includes a stylish haircut followed by a refreshing shampoo to leave your hair feeling clean and revitalized.",
-      price: "$20.00",
-    },
-    {
-      title: "Children’s Haircut",
-      description:
-        "A fun and friendly haircut experience for kids, ensuring they leave with a smile and a fresh new look.",
-      price: "$10.00",
-    },
-  ],
-  Grooming: [
-    {
-      title: "Beard Trim & Shape",
-      description:
-        "A precise beard trim and shape to maintain a clean and stylish look. Includes a consultation to achieve the perfect shape for your face.",
-      price: "$12.00",
-    },
-    {
-      title: "Hot Towel Shave",
-      description:
-        "A relaxing hot towel shave that softens the skin and beard for a smooth, close shave, leaving your skin refreshed and rejuvenated.",
-      price: "$18.00",
-    },
-    {
-      title: "Beard & Moustache Styling",
-      description:
-        "A detailed grooming service that includes trimming and styling of both beard and moustache, perfect for a well-groomed appearance.",
-      price: "$15.00",
-    },
-  ],
-  Treatments: [
-    {
-      title: "Scalp Massage",
-      description:
-        "A soothing scalp massage that helps relieve tension and promotes healthy hair growth. Perfect for stress relief and relaxation.",
-      price: "$10.00",
-    },
-    {
-      title: "Hair & Scalp Treatment",
-      description:
-        "A nourishing treatment that revitalizes both hair and scalp, addressing issues like dryness, dandruff, and hair loss.",
-      price: "$25.00",
-    },
-    {
-      title: "Eyebrow Shaping",
-      description:
-        "A precise eyebrow shaping service that enhances your facial features, ensuring a clean and sharp appearance.",
-      price: "$8.00",
-    },
-  ],
-};
+import {
+  useFetchServicesQuery,
+  useFetchServiceCategoriesQuery,
+} from "../../services/api";
 
 export default function ServicesSection() {
   const theme = useTheme();
   const isSmallScreen = useMediaQuery(theme.breakpoints.down("md"));
-  const [selectedTab, setSelectedTab] = useState("Haircut");
+  const [selectedTab, setSelectedTab] = useState(0); // Updated to be a number
+  const [servicesByCategory, setServicesByCategory] = useState({});
+
+  const { data: services = [] } = useFetchServicesQuery();
+  const { data: categories = [] } = useFetchServiceCategoriesQuery();
+
+  useEffect(() => {
+    if (services.length && categories.length) {
+      const organizedServices = categories.reduce((acc, category) => {
+        acc[category.name] = services.filter(
+          (service) => service.category === category.id
+        );
+        return acc;
+      }, {});
+      setServicesByCategory(organizedServices);
+    }
+  }, [services, categories]);
 
   const handleTabChange = (event, newValue) => {
     setSelectedTab(newValue);
@@ -112,55 +70,56 @@ export default function ServicesSection() {
             </Typography>
           </ServiceTitleContainer>
 
-          <ServiceTabContent>
-            <Box
-              sx={{
-                borderBottom: 1,
-                borderColor: "divider",
-                marginTop: "2rem",
-              }}
-            >
-              <CustomTabs>
-                <Tabs value={selectedTab} onChange={handleTabChange} centered>
-                  {Object.keys(servicesData).map((category) => (
-                    <Tab key={category} label={category} value={category} />
-                  ))}
-                </Tabs>
-              </CustomTabs>
-            </Box>
+          {services.length > 0 ? (
+            <ServiceTabContent>
+              <Box
+                sx={{
+                  borderBottom: 1,
+                  borderColor: "divider",
+                  marginTop: "2rem",
+                }}
+              >
+                <CustomTabs>
+                  <Tabs value={selectedTab} onChange={handleTabChange} centered>
+                    {Object.keys(servicesByCategory).map((category, index) => (
+                      <Tab key={category} label={category} value={index} />
+                    ))}
+                  </Tabs>
+                </CustomTabs>
+              </Box>
 
-            {Object.keys(servicesData).map((category) => (
-              <TabPanel key={category} value={selectedTab} index={category}>
-                <ServiceGrid>
-                  {servicesData[category].map((service, index) => (
-                    <ServiceItem key={index}>
-                      <ServiceTitle>{service.title}</ServiceTitle>
-                      <Typography variant="body2" color="textSecondary">
-                        {service.description}
-                      </Typography>
-                      <ServicePrice>{service.price}</ServicePrice>
-                      <Box sx={{ flexGrow: 1 }} />{" "}
-                      {/* Spacer to push the button down */}
-                      <Link href="#booking-section" underline="none">
-                        <BookButton variant="contained">Book now</BookButton>
-                      </Link>
-                    </ServiceItem>
-                  ))}
-                </ServiceGrid>
-              </TabPanel>
-            ))}
-          </ServiceTabContent>
+              {Object.keys(servicesByCategory).map((category, index) => (
+                <TabPanel key={category} value={selectedTab} index={index}>
+                  <ServiceGrid>
+                    {servicesByCategory[category].map((service) => (
+                      <ServiceItem key={service.id}>
+                        <ServiceTitle>{service.title}</ServiceTitle>
+                        <Typography variant="body2" color="textSecondary">
+                          {service.description}
+                        </Typography>
+                        <ServicePrice>{`$${service.price.toFixed(
+                          2
+                        )}`}</ServicePrice>
+                        <Box sx={{ flexGrow: 1 }} />
+                        <Link href="#booking-section" underline="none">
+                          <BookButton variant="contained">Book now</BookButton>
+                        </Link>
+                      </ServiceItem>
+                    ))}
+                  </ServiceGrid>
+                </TabPanel>
+              ))}
+            </ServiceTabContent>
+          ) : (
+            <Alert severity="warning">
+              There are no services available in the database.
+            </Alert>
+          )}
         </Container>
       </ScrollAnimation>
     </ServicesContainer>
   );
 }
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.number.isRequired,
-  value: PropTypes.number.isRequired,
-};
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -177,3 +136,9 @@ function TabPanel(props) {
     </div>
   );
 }
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
