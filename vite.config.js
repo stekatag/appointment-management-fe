ad04c6e92a665ee69af40c458eaf5a1c0fb53430
Stefan Gogov <stefangogov@proton.me>
2024-08-29 22:52:48 +0300

    chore: Update npm dependencies and add vite-plugin-pwa for progressive web app support

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,59 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
+      manifest: {
+        name: "Barbershop App",
+        short_name: "Barbershop",
+        description: "Barbershop management system for small businesses",
+        theme_color: "#AF8447",
+        background_color: "#f7f7f7",
+        scope: "/",
+        start_url: "/",
+        display: "standalone",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "/apple-touch-icon.png",
+            sizes: "180x180",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+        screenshots: [
+          {
+            src: "/screenshots/screenshot1.jpg",
+            sizes: "1921x884",
+            type: "image/png",
+            form_factor: "wide",
+          },
+          {
+            src: "/screenshots/screenshot2.png",
+            sizes: "432x935",
+            type: "image/png",
+            form_factor: "narrow",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@mui/styled-engine": "@mui/styled-engine-sc",
