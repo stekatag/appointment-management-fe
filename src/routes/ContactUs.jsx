febb3aec809a2bae5daf4d755a89e2ee24cd4f15
Stefan Gogov <stefangogov@proton.me>
2024-09-03 17:57:46 +0300

    refactor: Update ContactUs and ContactSection to use new ContactSection component

--- src/routes/ContactUs.jsx
+++ src/routes/ContactUs.jsx
@@ -1,13 +1,14 @@
 import BreadcrumbsComponent from "../components/BreadcrumbsComponent/BreadcrumbsComponent";
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
+import ContactSection from "../sections/ContactSection/ContactSection";
 
 export default function ContactUs() {
   return (
     <>
       <Header />
       <BreadcrumbsComponent />
-      <h1>Contact Us</h1>
+      <ContactSection />
       <Footer />
     </>
   );
